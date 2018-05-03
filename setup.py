5e19d538d4be41472ad411c250afa0db9646368d
Dylan Storey <dylan.storey@gmail.com>
2018-05-03 22:45:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import sys
 import setuptools
 import os
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 # noinspection PyProtectedMember
 from pip.download import PipSession
 
