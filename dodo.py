27d479136a56de5032af908a1ba0f3bce2d2ea9c
Dylan Storey <dylan.storey@gmail.com>
2018-07-11 22:01:46 -0400

    updated docs

--- dodo.py
+++ dodo.py
@@ -1,4 +1,5 @@
 import doit
+import fnmatch
 import os
 import shutil
 import glob
@@ -70,4 +71,56 @@ def task_doc_coverage():
     os.environ['SPHINX_APIDOC_OPTIONS']='members'
     return {
         'actions' : ['sphinx-apidoc -fMTeE -o docs/source angreal/ && cd docs && make coverage && cat build/coverage/python.txt >&2'] ,
+}
+
+
+def task_loc():
+    """
+    Get current LOC in project
+    :return:git
+    """
+
+    def walk(root='.', recurse=True, pattern='*'):
+        """
+            Generator for walking a directory tree.
+            Starts at specified root folder, returning files
+            that match our pattern. Optionally will also
+            recurse through sub-folders.
+        """
+        for path, subdirs, files in os.walk(root):
+            for name in files:
+                if fnmatch.fnmatch(name, pattern):
+                    yield os.path.join(path, name)
+            if not recurse:
+                break
+
+    def loc(root='', recurse=True):
+        """
+            Counts lines of code in two ways:
+                maximal size (source LOC) with blank lines and comments
+                minimal size (logical LOC) stripping same
+
+            Sums all Python files in the specified folder.
+            By default recurses through subfolders.
+        """
+        count_mini, count_maxi = 0, 0
+        for fspec in walk(root, recurse, '*.py'):
+            skip = False
+            for line in open(fspec).readlines():
+                count_maxi += 1
+
+                line = line.strip()
+                if line:
+                    if line.startswith('#'):
+                        continue
+                    if line.startswith('"""'):
+                        skip = not skip
+                        continue
+                    if not skip:
+                        count_mini += 1
+
+        return "\'Lines of Code: {0} \nTotal Lines: {1}\'".format(count_mini, count_maxi)
+
+    return {
+        'actions' : ['echo %s >&2 ' % loc('.')],
 }
