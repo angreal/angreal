df1b5c116d820212e21ee2a680ff9bca5542c6b0
Dylan Storey <dylan.storey.e15o@statefarm.com>
2018-07-12 14:43:14 -0400

    re-organized tests to ensure that they match the project structure

--- angreal/decorators/__init__.py
+++ angreal/decorators/__init__.py
@@ -7,3 +7,5 @@ from click.decorators import pass_context, pass_obj, make_pass_decorator, \
 # Utilities
 from click.utils import echo, get_binary_stream, get_text_stream, open_file, \
      format_filename, get_app_dir, get_os_args
+
+from angreal.integrations.doit import doit_task
