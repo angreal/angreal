ffdb840be300853f408ed0aa7a7683102c32c79f
dylanbstorey <dstorey@repay.com>
2019-05-20 12:53:21 -0400

    i think home is breaking ish

--- angreal/cutter.py
+++ angreal/cutter.py
@@ -56,6 +56,7 @@ def initialize_cutter(template, **kwargs):
     angreal_hidden = os.path.join(project_path, '.angreal')
     generated_replay = os.path.join(os.environ.get('HOME'), '.cookiecutter_replay', '{}.json'.format(template_name))
 
+    assert os.path.isfile(generated_replay)
 
 
     os.makedirs(angreal_hidden, exist_ok=True)
