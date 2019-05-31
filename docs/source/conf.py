0d672b2508891d5dbbe165d58e175b65d5fd9b58
Dylan Storey <dylan.storey@gmail.com>
2019-05-30 22:39:25 -0400

    attempting to change the cannonical url

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -117,62 +117,6 @@ html_sidebars = {
     ]
 }
 
-
-# -- Options for HTMLHelp output ------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'angrealdoc'
-
-
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'angreal.tex', 'angreal Documentation',
-     'Dylan Storey', 'manual'),
-]
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'angreal', 'angreal Documentation',
-     [author], 1)
-]
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'angreal', 'angreal Documentation',
-     author, 'angreal', 'One line description of project.',
-     'Miscellaneous'),
-]
-
-
-
+html_theme_options = {
+    'canonical_url': 'https://angreal.gitlab.io'
+    }
