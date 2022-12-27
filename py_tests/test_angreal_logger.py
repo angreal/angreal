379301d73563be5a30264f9d5b0bd65a1adda925
dstorey <dylan.storey@gmail.com>
2022-12-26 21:52:49 -0500

    logging pytests updated

--- py_tests/test_angreal_logger.py
+++
@@ -1,9 +0,0 @@
-import angreal
-import logging
-
-def test_debug():
-    logging.debug("test")
-    logging.error("test")
-    logging.info("test")
-    logging.warning("test")
-    logging.log(0,"test")
