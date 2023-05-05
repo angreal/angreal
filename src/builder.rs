85b0eff961dea760e75b419c46bd2e3b3422e579
Dylan Storey <dylan.storey@gmail.com>
2023-05-05 19:48:29 -0400

    stopping point for Ms Maisel

--- src/builder.rs
+++ src/builder.rs
@@ -1,6 +1,8 @@
 //! The angreal app builder
 //!
-use crate::task::{AngrealArg, ANGREAL_ARGS, ANGREAL_TASKS};
+use std::ops::Not;
+
+use crate::task::{AngrealArg, ANGREAL_ARGS, ANGREAL_TASKS, ANGREAL_GROUPS};
 use clap::{App, AppSettings, Arg, ArgAction, Command};
 
 /// Get the args for a given command.
@@ -16,7 +18,7 @@ pub fn select_args(name: String) -> Vec<AngrealArg> {
 /// Build the final CLI from the registered tasks
 pub fn build_app(in_angreal_project: bool) -> App<'static> {
     // Build the initial App with angreal sub commands
-    let mut app = App::new("angreal")
+    let mut app = Command::new("angreal")
         .setting(AppSettings::NoBinaryName)
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .arg(
@@ -53,35 +55,92 @@ pub fn build_app(in_angreal_project: bool) -> App<'static> {
                         )
                     )
     }
-
-    let commands = ANGREAL_TASKS.lock().unwrap().clone();
-    for cmd in commands.into_iter() {
-        let mut sc = Command::new(cmd.name.as_str());
-        attr_copy_str!(sc, about, cmd);
-        attr_copy_str!(sc, long_about, cmd);
-
-        let args = select_args(cmd.name.clone());
-        for arg in args.into_iter() {
-            let name = &*Box::leak(Box::new(arg.name));
-
-            let mut a = Arg::new(name.as_str());
-
-            attr_copy_bool!(a, takes_value, arg);
-            attr_copy_str!(a, default_value, arg);
-            attr_copy_bool!(a, require_equals, arg);
-            attr_copy_bool!(a, multiple_values, arg);
-            attr_copy_u64!(a, number_of_values, arg);
-            attr_copy_u64!(a, max_values, arg);
-            attr_copy_u64!(a, min_values, arg);
-            attr_copy_char!(a, short, arg);
-            attr_copy_str!(a, long, arg);
-            attr_copy_str!(a, long_help, arg);
-            attr_copy_str!(a, help, arg);
-            attr_copy_bool!(a, required, arg);
-            sc = sc.arg(a);
+    else {
+        /// Generate groups into commands
+        let mut top_level_groups = Vec::new();
+        let mut groups = Vec::new();
+        let registered_groups = ANGREAL_GROUPS.lock().unwrap().clone();
+        for group in registered_groups.into_iter() {
+            let mut cmd = Command::new(group.name.as_str());
+            attr_copy_str!(cmd, about, group);
+            let cmd = cmd.setting(AppSettings::SubcommandRequiredElseHelp);
+            groups.push(cmd); 
         }
+        /// Generate all tasks into commands
 
-        app = app.subcommand(sc);
+        let registered_tasks = ANGREAL_TASKS.lock().unwrap().clone();
+        for cmd in registered_tasks.into_iter(){
+            let mut task = Command::new(cmd.name.as_str());
+            attr_copy_str!(task, about, cmd);
+            attr_copy_str!(task, long_about, cmd);
+
+            let args = select_args(cmd.name.clone());
+            for arg in args.into_iter() {
+                let name = &*Box::leak(Box::new(arg.name));
+
+                let mut a = Arg::new(name.as_str());
+                attr_copy_bool!(a, takes_value, arg);
+                attr_copy_str!(a, default_value, arg);
+                attr_copy_bool!(a, require_equals, arg);
+                attr_copy_bool!(a, multiple_values, arg);
+                attr_copy_u64!(a, number_of_values, arg);
+                attr_copy_u64!(a, max_values, arg);
+                attr_copy_u64!(a, min_values, arg);
+                attr_copy_char!(a, short, arg);
+                attr_copy_str!(a, long, arg);
+                attr_copy_str!(a, long_help, arg);
+                attr_copy_str!(a, help, arg);
+                attr_copy_bool!(a, required, arg);
+                task = task.arg(a);
+            }
+            
+            if cmd.group.is_none(){
+                // no group , register with `app`
+                
+                app = app.subcommand(task);
+            }
+            else{
+                
+                let mut c_groups = cmd.group.clone().unwrap();
+
+                let task_parent_pos =groups.iter().position(|x| x.get_name() == c_groups.last().unwrap().name.as_str()).unwrap(); 
+                let mut task_parent = groups[task_parent_pos].clone();
+                task_parent = task_parent.subcommand(task);
+                groups.insert(task_parent_pos, task_parent);
+
+                for (pos,e) in c_groups.iter().enumerate().rev(){
+                    
+                    if pos == 0{
+                        top_level_groups.push(e.name.clone());
+                        break;
+                    }
+
+                    let this_subcommand_pos = groups.iter().position(|x| x.get_name() == e.name.as_str()).unwrap();
+                    let parent_subcommand_pos =groups.iter().position(|x| x.get_name() == c_groups[pos-1].name.as_str()).unwrap(); 
+
+                    let this_subcommand = groups[this_subcommand_pos].clone();
+                    let mut parent_subcommand = groups[parent_subcommand_pos].clone();
+
+                    parent_subcommand = parent_subcommand.subcommand(this_subcommand);
+
+                    groups.insert(parent_subcommand_pos, parent_subcommand);
+
+                    
+
+            
+                }
+            
+                // loop groups backwards, register up the chain with a copy
+            }
+        }
+        
+            
+        
+        
+        
+        // Register all "top level commands"
+
+
     }
     app
 }
