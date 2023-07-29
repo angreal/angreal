39fe6d0c1d65e206186a58d6e7b3bb8524d597e6
Dylan Storey <dylan.storey@gmail.com>
2023-07-28 21:31:31 -0400

    added functional tests

---
+++ py_tests/functional_tests/.angreal/task_testing.py
@@ -0,0 +1,29 @@
+import angreal
+import os
+import subprocess
+
+
+
+group1 = angreal.command_group(name="group1", about="testing group")
+
+
+@group1()
+@angreal.command(name="flag", about="test a flag argument")
+@angreal.argument(name="test", long="test", short="t", takes_value=False, is_flag=True)
+def flag_tests(test):
+    """
+    """
+    if test:
+        open("group.txt","w").close()
+
+
+@group1()
+@angreal.group(name="group2", about="group2")
+@angreal.command(name="flag2", about="test a flag argument")
+@angreal.argument(name="test", long="test", short="t", takes_value=False, is_flag=True)
+def flag_test2(test):
+    """
+    """
+    if test:
+        open("nested_group.txt","w").close()
+
---
+++ py_tests/test_functional.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+
+here = os.path.dirname(__file__)
+functional_test_folder = os.path.join(here,"functional_tests")
+
+
+def test_group_1():
+    """test a basic nested command with flag"""
+    rv = subprocess.run([
+        "angreal",
+        "group1",
+        "flag",
+        "-t"
+    ], cwd=functional_test_folder)
+    try:
+        assert rv.returncode == 0
+        assert os.path.exists(os.path.join(functional_test_folder,"group.txt"))
+    except Exception as e:
+        raise e
+    finally:
+        try:
+            os.unlink(os.path.join(functional_test_folder,"group.txt"))
+        except:
+            pass
+    pass
+
+
+def test_group_2():
+    """test a multi nested command with flag"""
+    rv = subprocess.run([
+        "angreal",
+        "group1",
+        "group2",
+        "flag2",
+        "-t"
+    ], cwd=functional_test_folder)
+    try:
+        assert rv.returncode == 0
+        assert os.path.exists(os.path.join(functional_test_folder,"nested_group.txt"))
+    except Exception as e:
+        raise e
+    finally:
+        try:
+            os.unlink(os.path.join(functional_test_folder,group.txt))
+        except:
+            pass
+    pass
